277f7900e94a71249988c67c54a315c8653b3118
Timothy Vieira <vieira.timothy@yahoo.ca>
2021-08-09 21:23:03 -0400

    Updated messages

--- src/App.js
+++ src/App.js
@@ -2,20 +2,34 @@ import './App.css';
 import Header from './Header';
 import TinderCards from './TinderCards';
 import SwipeButtons from './SwipeButtons';
+import Chats from './Chats';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ChatScreen from './ChatScreen';
 
 function App() {
   return (
     <div className="app">
-
+      <Router>
+        <Switch>
+          <Route path="/chat/:person">
+            <Header backButton="/chat"/>
+            <ChatScreen />
+          </Route>
+          <Route path="/chat">
+            <Header backButton="/" />
+            <Chats />
+          </Route>
+          <Route path="/">
+            {/* TinderCards */}
+            <Header />
+            <TinderCards />
+            {/* SwipeButtons */}
+            <SwipeButtons />
+          </Route>
+        </Switch>
+      </Router>
       {/* Header */}
-      <Header />
 
-      {/* TinderCards */}
-      <TinderCards />
-
-
-      {/* SwipeButtons */}
-      <SwipeButtons />
     </div> 
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -3,22 +3,35 @@ import "./Header.css";
 import PersonIcon from '@material-ui/icons/Person';
 import IconButton from '@material-ui/core/IconButton';
 import ForumIcon from '@material-ui/icons/Forum';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { Link, useHistory } from 'react-router-dom';
 
-function Header() {
+function Header({ backButton }) {
+    const history = useHistory();
     return (
         <div className='header'>
-            {/* <h2>I am a header</h2> */}
-            <IconButton>
-                <PersonIcon fontSize="large" className='header__icon'/>
-            </IconButton>
-            <img
-                className="header__logo"
-                src="https://logos-world.net/wp-content/uploads/2020/09/Tinder-Emblem.png"
-                alt="tinder logo"
-            />
-            <IconButton>
-                <ForumIcon fontSize="large" className='header__icon'/>
-            </IconButton>
+            {backButton ? (
+                <IconButton onClick={() => history.replace(backButton)}>
+                    <ArrowBackIosIcon fontSize="large" className="header__icon" />
+                </IconButton>
+            ) : (
+                <IconButton>
+                    <PersonIcon fontSize="large" className='header__icon'/>
+                </IconButton>
+            )}
+            <Link to="/">
+                <img
+                    className="header__logo"
+                    src="https://logos-world.net/wp-content/uploads/2020/09/Tinder-Emblem.png"
+                    alt="tinder logo"
+                />
+
+            </Link>
+            <Link to="/chat">
+                <IconButton>
+                    <ForumIcon fontSize="large" className='header__icon'/>
+                </IconButton>
+            </Link>
         </div>
     )
 }
